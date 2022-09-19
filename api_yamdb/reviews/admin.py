8c90fe9423a9ac8b50038aaded09eb438a2b28f0
Ilya Valeev <iljavaleev@gmail.com>
2022-09-19 19:46:10 +0500

    isort

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Genre, Category, Title, Comment, Review
+from .models import Category, Comment, Genre, Review, Title
 
 admin.site.register(Title)
 admin.site.register(Category)
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,7 +1,7 @@
 import datetime
 
-from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 User = get_user_model()
